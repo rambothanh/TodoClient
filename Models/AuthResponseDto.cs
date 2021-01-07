e83dee7d9f6549481b5bcb1fb2dd27c7bbf673f7
Nguyen Trong Thanh <rambothanh1985@gmail.com>
2021-01-07 16:14:52 +0700

    Add class

---
+++ Models/AuthResponseDto.cs
@@ -0,0 +1,13 @@
+namespace TodoClient.Models {
+    public class AuthRespenseDto {
+        public string Id { get; set; }
+        public string Username { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    
+        public string Token { get; set; }
+        public string Message { get; set; }
+
+    }
+   
+}
