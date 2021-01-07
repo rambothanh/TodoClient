0f24de6e5ef306191bec8becaafaebfe5433461a
Nguyen Trong Thanh <rambothanh1985@gmail.com>
2021-01-07 16:35:22 +0700

    add Blazored.LocalStorage, add Microsoft.AspNetCore.Components.Authorization

--- Services/AuthenticationService/AuthenticationService.cs
+++ Services/AuthenticationService/AuthenticationService.cs
@@ -1,9 +1,22 @@
 using System.Threading.Tasks;
 using TodoClient.Models;
+using System.Net.Http;
+using Microsoft.AspNetCore.Components.Authorization;
+using Blazored.LocalStorage;
+
 namespace TodoClient.Services.AuthenticationService
 {
     public class AuthenticationService : IAuthenticationService
     {
+        private readonly HttpClient _client;
+        private readonly AuthenticationStateProvider _authStateProvider;
+        private readonly ILocalStorageService _localStorage;
+        public AuthenticationService(HttpClient client, AuthenticationStateProvider authStateProvider, ILocalStorageService localStorage)
+        {
+            _client = client;
+            _authStateProvider = authStateProvider;
+            _localStorage = localStorage;
+        }
         public Task<AuthResponseDto> Login(AuthenticateModel userForAuthentication)
         {
             throw new System.NotImplementedException();
