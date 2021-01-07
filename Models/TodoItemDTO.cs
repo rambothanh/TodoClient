c60c69959fa8a10a23c6b298ff948fe00734c8b0
Nguyen Trong Thanh <rambothanh1985@gmail.com>
2021-01-07 13:25:23 +0700

    update: Edit Name and edit IsComplete

--- Models/TodoItemDTO.cs
+++ Models/TodoItemDTO.cs
@@ -9,6 +9,7 @@ namespace TodoClient.Models
     {
         public long Id { get; set; }
         public string Name { get; set; }
-        public bool IsComplete { get; set; }
+        public bool IsComplete { get; set; } = false;
+        public bool Editing { get; set; } = false;
     }
 }
